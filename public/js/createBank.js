0c6cc8eab290a68a476abce89b6e5ce9acff60da
Jason <88212653+raw-chicken@users.noreply.github.com>
2022-04-19 14:27:40 -0400

    monkey see monkey do

---
+++ public/js/createBank.js
@@ -0,0 +1,38 @@
+let registerButton = document.getElementById("create")
+let bid = document.getElementById("id")
+let bankName = document.getElementById("name")
+let street = document.getElementById("street")
+let city = document.getElementById("city")
+let state = document.getElementById("state")
+let zip = document.getElementById("zip")
+let reserved = document.getElementById("reserved_assets")
+
+function addBank(event) {
+    //event.preventDefault()
+    let xhr = new XMLHttpRequest
+    xhr.addEventListener("load", responseHandler)
+    query = `bid=${bid.value}&name=${bankName.value}&street=${street.value}&city=${city.value}&state=${state.value}&zip=${zip.value}&reserved=${reserved.value}`
+    //console.log(query)
+    // when submitting a GET request, the query string is appended to URL
+    // but in a POST request, do not attach the query string to the url
+    // instead pass it as a parameter in xhr.send()
+    url = `/addCorporation`
+    xhr.responseType = "json";
+    xhr.open("POST", url)
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+    // notice the query string is passed as a parameter in xhr.send()
+    // this is to prevent the data from being easily sniffed
+    xhr.send(query)
+}
+
+function responseHandler() {
+    //let message = document.getElementById("message")
+   // message.style.display = "block"
+    if (this.response.success) {
+        console.log(this.response.message);
+    } else {
+        console.log(this.response.success)
+      //  message.innerText = this.response.message
+    }
+}
+registerButton.addEventListener("click", addBank)
